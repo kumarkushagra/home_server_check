b7acae8b24333b75be656e0914843659c6f675de
Kushagra Kumar <64472949+kumarkushagra@users.noreply.github.com>
2025-05-28 08:28:44 +0000

    Update new code

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    def fetch_data(url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an error for bad responses
+            return "online"  # Assuming the response is in JSON format
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            return "offline"
+    return fetch_data("ssh.devvdeploy.site")
